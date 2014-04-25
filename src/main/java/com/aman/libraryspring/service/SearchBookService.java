a66e251905ca5cac872276b4eace4a3dd5456f5b
Aman Kumar <amankumar@Amans-MacBook-Pro.local>
2014-04-25 07:43:36 +0530

    added functionality for default search, changes to display is pending

--- src/main/java/com/aman/libraryspring/service/SearchBookService.java
+++ src/main/java/com/aman/libraryspring/service/SearchBookService.java
@@ -33,22 +33,7 @@ public class SearchBookService {
 	*/
 	List<Record> list=new ArrayList<Record>();
 	public List<Record> search(Book book) {
-		/*
-		int i= bookDao.searchBook(book).size();
-		System.out.println("inside searchBookService");
-		for(int k=0;k<i;k++){
-			System.out.println("inside for loop of searchBook Service checking bookDAORETURN");
-			System.out.println("Book returned: "+bookDao.searchBook(book).get(k));
-			list=recordDao.getRecordList(bookDao.searchBook(book).get(k));
-		}
-		*/
-		//System.out.println("inside searchBookService");
-		//System.out.println("returned size of record list: "+i);
-		//for(int j=0;j<i;j++){
-			//list=recordDao.getRecordList(bookDao.searchBook(book).get(j));
-			//System.out.println("inside second search service loop: " +list.get(j));
-			//System.out.println("inside second search service loop, List size: " +list.size());
-		//}
+		
 		if((book.getAuthor() == "") && (book.getName() == "") ){
 			return recordDao.listRecord();
 		}else{
